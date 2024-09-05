2aa79f0138e4f95b8f74de811fa37f5a54d86f7a
Raquel Sato <raquelsato1@gmail.com>
2024-09-05 10:50:00 -0300

    feat: add password strength logic

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const mediumBox = document.querySelector(".medium");
   const strongBox = document.querySelector(".strong");
   const passwordText = document.querySelector(".password-text");
+  const generatePasswordButtons = document.querySelectorAll(".generate-new-password");
 
   textInput.value = rangeInput.value;
 
@@ -12,9 +13,64 @@ document.addEventListener("DOMContentLoaded", () => {
     textInput.value = rangeInput.value;
   });
 
-  const generatePassword = document.getElementById("generate-new-password");
-
   function generateRandomPassword(length, includeUppercase, includeLowercase, includeNumbers, includeSymbols) {
+    const requirementsPassword = [includeUppercase, includeLowercase, includeNumbers, includeSymbols];
+    if (requirementsPassword.every((requirement) => !Boolean(requirement))) return;
+
+    let forcePoint = requirementsPassword.reduce((accPoint, requirement) => {
+      let point = accPoint;
+      if (!!requirement) {
+        point += 2;
+      }
+      return point;
+    }, 0);
+
+    if (Number(length) === 7) {
+      forcePoint += 1;
+    }
+    if (Number(length) > 7 && length <= 8) {
+      forcePoint += 2;
+    }
+    if (Number(length) >= 9 && length <= 16) {
+      forcePoint += 3;
+    }
+    if (Number(length) >= 17) {
+      forcePoint += 4;
+    }
+
+    if (forcePoint <= 5) {
+      weakBox.classList.remove("green");
+      mediumBox.classList.remove("green");
+      strongBox.classList.remove("green");
+
+      weakBox.classList.remove("yellow");
+      mediumBox.classList.remove("yellow");
+
+      weakBox.classList.add("red");
+    }
+
+    if (forcePoint >= 6 && forcePoint <= 7) {
+      weakBox.classList.remove("green");
+      mediumBox.classList.remove("green");
+      strongBox.classList.remove("green");
+
+      weakBox.classList.remove("red");
+      mediumBox.classList.remove("red");
+      strongBox.classList.remove("red");
+
+      weakBox.classList.remove("green");
+      weakBox.classList.remove("green");
+
+      weakBox.classList.add("yellow");
+      mediumBox.classList.add("yellow");
+    }
+
+    if (forcePoint >= 8) {
+      weakBox.classList.add("green");
+      mediumBox.classList.add("green");
+      strongBox.classList.add("green");
+    }
+
     const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
     const numberChars = "0123456789";
@@ -42,18 +98,19 @@ document.addEventListener("DOMContentLoaded", () => {
     passwordText.textContent = password;
   }
 
-  generatePassword.addEventListener("click", () => {
-    const numberOfCharacters = document.querySelector(".input-typed").value;
-    const rangeValue = document.querySelector(".input-range").value;
-    const includeUppercase = document.getElementById("uppercase").checked;
-    const includeLowercase = document.getElementById("lowercase").checked;
-    const includeNumbers = document.getElementById("numbers").checked;
-    const includeSymbols = document.getElementById("symbols").checked;
-    generateRandomPassword(numberOfCharacters, includeUppercase, includeLowercase, includeNumbers, includeSymbols);
-    if ([includeUppercase, includeLowercase, includeNumbers, includeSymbols].every(Boolean)) {
-      weakBox.classList.add("green");
-      mediumBox.classList.add("green");
-      strongBox.classList.add("green");
-    }
+  generatePasswordButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const numberOfCharacters = document.querySelector(".input-typed").value;
+      const includeUppercase = document.getElementById("uppercase").checked;
+      const includeLowercase = document.getElementById("lowercase").checked;
+      const includeNumbers = document.getElementById("numbers").checked;
+      const includeSymbols = document.getElementById("symbols").checked;
+      generateRandomPassword(numberOfCharacters, includeUppercase, includeLowercase, includeNumbers, includeSymbols);
+      if ([includeUppercase, includeLowercase, includeNumbers, includeSymbols].every(Boolean)) {
+        weakBox.classList.add("green");
+        mediumBox.classList.add("green");
+        strongBox.classList.add("green");
+      }
+    });
   });
 });
